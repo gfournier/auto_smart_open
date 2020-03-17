83d62abd6f030a6d63d23b334c4b45c352468087
Félix <someonefefe@gmail.com>
2020-03-17 16:19:53 +0100

    fixing tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,22 +1,24 @@
 import pytest
-from unittest.mock import MagicMock
+from unittest.mock import patch
 
 from auto_smart_open.core import FileStorageBackend
 
 class Mocker:
     def __enter__(self):
+        print("entering context")
         return "entering context"
     def __exit__(self, type, value, traceback):
         raise FileNotFoundError
 
-def test_sopen():
+@patch('smart_open.open')
+def test_sopen(patched_sopen):
     """
     Replace the smart_open open with a mock function to see if __enter__ and __exit__ methods are properly called.
     """
+    print("test")
     backend = FileStorageBackend()
     # mock the smart open open
-    backend.open = MagicMock(return_value=Mocker())
+    patched_sopen.return_value = Mocker()
     with pytest.raises(FileNotFoundError):
         with backend.open("test", "r") as f:
             assert f == "entering context"
-        
